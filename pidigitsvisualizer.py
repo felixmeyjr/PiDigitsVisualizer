47f662814671725afcdd74316cb399b62fae090a
Felix <felix.meyer97@web.de>
2021-03-15 18:49:36 +0100

    init commit

---
+++ pidigitsvisualizer.py
@@ -0,0 +1,31 @@
+# PI digits distribution/occurrence visualizer
+
+"""
+Get digits of pi
+Calculate distribution for given interval
+Plot
+Repeat for a nice flow
+
+Later:
+User Input: how many digits?
+
+"""
+
+
+def calculateDigits():
+    pass
+
+
+def digitsDistribution():
+    pass
+
+
+def plot():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
