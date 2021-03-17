61f2fceb9d6feb0ddaddb910911f6bc361dabe56
Felix <felix.meyer97@web.de>
2021-03-17 18:35:43 +0100

    added non dynamic plot

--- pidigitsvisualizer.py
+++ pidigitsvisualizer.py
@@ -11,22 +11,37 @@ User Input: how many digits?
 
 """
 
-import math
-import matplotlib
+
+from mpmath import mp
+import matplotlib.pyplot as plt
 
 
 def calculateDigits(limit):
-    digits = list(str(round(math.pi, limit + 2)))[2:]
-    return digits
+    # Using multi precision
+    mp.dps = limit
+
+    return list(str(mp.pi))[2:]
 
 
 def digitsDistribution(digits):
-    pass
+    # distribution = {1: 6, 2: 7, ...}, so {digit: n times in pi}
+    distribution = {int(x): digits.count(x) for x in digits}
+
+    distribution = dict(sorted(distribution.items()))
+
+    return distribution
 
 
-def plot():
-    pass
+def plot(distribution):
+    plt.bar(range(len(distribution)), list(distribution.values()), align="center")
+    plt.xticks(range(len(distribution)), list(distribution.keys()))
+    plt.show()
 
 
 if __name__ == '__main__':
-    print(calculateDigits(10))
+    d = calculateDigits(10000)
+    # print(len(d))
+    print(d)
+    d1 = digitsDistribution(d)
+    print(d1)
+    plot(d1)
