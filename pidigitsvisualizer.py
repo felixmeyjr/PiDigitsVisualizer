e940df34a07847cc4de92b903d5e27e2076af2ed
Felix <felix.meyer97@web.de>
2021-03-18 10:32:34 +0100

    added min and max color

--- pidigitsvisualizer.py
+++ pidigitsvisualizer.py
@@ -11,14 +11,14 @@ User Input: how many digits?
 
 """
 
-
 from mpmath import mp
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 def calculateDigits(limit):
     # Using multi precision
-    mp.dps = limit
+    mp.dps = limit  # dps: decimal places
 
     return list(str(mp.pi))[2:]
 
@@ -27,21 +27,37 @@ def digitsDistribution(digits):
     # distribution = {1: 6, 2: 7, ...}, so {digit: n times in pi}
     distribution = {int(x): digits.count(x) for x in digits}
 
-    distribution = dict(sorted(distribution.items()))
-
-    return distribution
+    return dict(sorted(distribution.items()))
 
 
 def plot(distribution):
-    plt.bar(range(len(distribution)), list(distribution.values()), align="center")
+    # Highest n of occ should be green; lowest red
+    clrs = []
+    for val in distribution.values():
+        if val == min(distribution.values()):
+            clrs.append("red")
+        elif val == max(distribution.values()):
+            clrs.append("green")
+        else:
+            clrs.append("blue")
+
+    plt.bar(range(len(distribution)), list(distribution.values()), color=clrs, align="center")
     plt.xticks(range(len(distribution)), list(distribution.keys()))
+
+    plt.xlabel("Digit")
+    plt.ylabel("Number of occurrences")
+    plt.title("Distribution of pi digits")
     plt.show()
 
 
-if __name__ == '__main__':
+def main():
     d = calculateDigits(100)
     # print(len(d))
     print(d)
     d1 = digitsDistribution(d)
     print(d1)
     plot(d1)
+
+
+if __name__ == '__main__':
+    main()
